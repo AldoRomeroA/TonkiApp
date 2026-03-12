f0c76d150bf293c6fb41bd5b304fb162c1edb403
RaulHdezAvi3101 <nkh31r@gmail.com>
2026-03-12 01:13:38 -0600

    initial modular dashboard UI structure with Navbar and Footer - Implement initial Admin Dashboard layout using Next.js

---
+++ .next/dev/static/chunks/tonkiapp_Dashboard_app_layout_4cc561cb.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/tonkiapp_Dashboard_92b064d0._.css",
+  "static/chunks/42da5_next_dist_4ed7bd7a._.js"
+],
+    source: "dynamic"
+});
---
+++ app/dashboard/layout.js
@@ -0,0 +1,1 @@
+../../components/layout/navbar/navbar../../components/ui/dropdown
